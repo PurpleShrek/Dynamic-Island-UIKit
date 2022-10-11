27f79d6f16efc8a131a53355db8d7a5b91d16e3a
Purple Shrek <63758848+PurpleShrek@users.noreply.github.com>
2022-10-11 20:19:42 +0300

    second commit

--- DynamicIslandUIKitDemo/AppDelegate.swift
+++ DynamicIslandUIKitDemo/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         showVC()
-        print("test")
         return true
     }
     
